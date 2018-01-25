b7bcd21bbc268ddb0659c4cbf10dcb76c58e0c39
jpdemory <jpdemory@manastria.fr>
2018-01-25 09:20:42 +0100

    Affichage simple de la table

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Un petit programme qui affiche la table des 8
+            // Opérateur pout concaténer : +
+            // Saut de ligne : \n
+
+            const int multiple = 8;
+            const int valMax = 10;
+
+            string affichage = "";
+
+            // Constitution de la table
+            for (int i=0; i <= valMax; i++)
+            {
+                // affichage = affichage + i;
+                affichage += i + " * " + multiple + " = " + i * multiple;
+                affichage += "\n";
+            }
+
+            Console.WriteLine(affichage);
+
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+        }
+    }
+}
